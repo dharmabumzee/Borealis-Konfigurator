2d251d61ea432b167f2453b5b88e04660fbacd42
lukabelani <luka.belani@gmail.com>
2020-12-09 00:06:37 +0100

    refactor services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,28 +6,23 @@ export const Services = ({
   handleChange,
   checkedItems,
 }) => {
+  const renderCheckbox = () => {
+    return services.map((service) => (
+      <div key={service.id} className="service-checkbox">
+        <label key={service.key}>
+          <Checkbox
+            name={service.name}
+            checked={checkedItems[service.name]}
+            onChange={handleChange}
+            price={Number(service.price)}
+          />
+          {service.name} ({Math.trunc(service.price)} kn)
+        </label>
+      </div>
+    ));
+  };
+
   return (
-    <div
-      className="content-services"
-      style={{ marginTop: "50px", marginBottom: "150px" }}
-    >
-      {services.map((service) => (
-        <div
-          key={service.id}
-          // className="ui checkbox"
-          style={{ marginBottom: "15px" }}
-        >
-          <label key={service.key}>
-            <Checkbox
-              name={service.name}
-              checked={checkedItems[service.name]}
-              onChange={handleChange}
-              price={Number(service.price)}
-            />
-            {service.name} ({Math.trunc(service.price)} kn)
-          </label>
-        </div>
-      ))}
-    </div>
+    <div className="content-services render-checkbox">{renderCheckbox()}</div>
   );
 };
